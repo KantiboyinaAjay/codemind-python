e8369349845a179a35a59be9c53f3323d41f9a56
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-05-03 15:38:12 +0530

    x = int(input()) n = len(str(x)) b=x s=0 while x!=0:     v = x%10     x = x//10     m = v**n     s+=m     n-=1 if b==s:     print("True") else:     print("False")

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,14 +1,14 @@
-x = eval(input())
-b=x
+x = int(input())
 n = len(str(x))
+b=x
 s=0
 while x!=0:
     v = x%10
-    x=x//10
+    x = x//10
     m = v**n
     s+=m
     n-=1
-if s==b:
+if b==s:
     print("True")
 else:
     print("False")
