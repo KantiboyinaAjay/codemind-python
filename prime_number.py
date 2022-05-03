f07e9451d3388dc608b960dde7bcddb960951880
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-05-03 15:48:33 +0530

    x = eval(input()) f = 0 if x ==1:     print("prime") for i in range(2,x+1):     if x%i==0:         f+=1 if f==1:     print("prime") else:     print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,11 +1,11 @@
-y = eval(input())
-x = 1
-flag=0
-while(x<=y):
-    if(y%x==0):
-        flag+=1
-    x+=1
-if(flag==2):
+x = eval(input())
+f = 0
+if x ==1:
+    print("prime")
+for i in range(2,x+1):
+    if x%i==0:
+        f+=1
+if f==1:
     print("prime")
 else:
     print("not a prime")
