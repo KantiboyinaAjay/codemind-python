16724681c2d2014c595f695983df0d40ea398fe8
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-05-24 15:44:41 +0530

    x = int(input()) l = list(map(int,input().split())) k=[] c=0 for i in list(set(l)):     if l.count(i)==i:         k.append(i) if k == []:     print('-1') else:     print(min(k),end=" ")     print(max(k))

--- Maximum_and_Minimum.py
+++ Maximum_and_Minimum.py
@@ -1,17 +1,11 @@
 x = int(input())
 l = list(map(int,input().split()))
 k=[]
-n=1
 c=0
-for i in l:
-    for j in l:
-        if i==j:
-            c+=1
-    if c==i:
+for i in list(set(l)):
+    if l.count(i)==i:
         k.append(i)
-        n=0
-    c=0
-if k==[]:
+if k == []:
     print('-1')
 else:
     print(min(k),end=" ")
