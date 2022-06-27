4d97252b9b4122957a1e1beb7cf004104c976ced
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-06-27 21:26:58 +0530

    x , y = map(int,input().split()) for i in range(1,y+1):     n = x*i     if n%y==0:         print(n)         break

--- LCM.py
+++ LCM.py
@@ -1,10 +1,6 @@
 x , y = map(int,input().split())
-l=[]
-m = []
 for i in range(1,y+1):
-    n = i*x
-    l.append(n)
-for j in l:
-    if j%y==0:
-        print(j)
+    n = x*i
+    if n%y==0:
+        print(n)
         break
