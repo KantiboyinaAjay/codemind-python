788a007e075b5da9b62acf00b8d81e7c7ff25fca
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-06-27 21:35:30 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1,1):         if n%i==0:             return 0             break     return 1 x = int(input()) if prime(x):     n = str(x)     m = len(n)     c=0     for i in n:         if prime(int(i)):             c+=1     if c==m:         print('Mega Prime')     else:         print('Not Mega Prime')     c=0 else:     print('Not Mega Prime')

--- mega_prime.py
+++ mega_prime.py
@@ -1,20 +1,23 @@
-n=int(input())
-x=2
-c=0
-d=0
-while x<n//2:
-    if n%x==0:
-        c+=1
-    x+=1
-if c!=0:
-    print('Not Mega Prime')
-else:
-    while n:
-        if n%10==2 or n%10==3 or n%10==5 or n%10==7:
+def prime(n):
+    if n==1:
+        return 0
+    for i in range(2,int(n**0.5)+1,1):
+        if n%i==0:
+            return 0
+            break
+    return 1
+x = int(input())
+if prime(x):
+    n = str(x)
+    m = len(n)
+    c=0
+    for i in n:
+        if prime(int(i)):
             c+=1
-        d+=1
-        n//=10
-    if c==d:
-         print('Mega Prime')
+    if c==m:
+        print('Mega Prime')
     else:
-         print('Not Mega Prime')
+        print('Not Mega Prime')
+    c=0
+else:
+    print('Not Mega Prime')
