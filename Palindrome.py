70ca1eb88d2a78d8543a025192b9983b4a408d6b
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-06-27 21:59:06 +0530

    n = input() m = n[::-1] if m==n:     print(True) else:     print(False)

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,6 @@
-x = int(input())
-b=x
-s=0
-while x!=0:
-    v = x%10
-    s = s*10+v
-    x = x//10
-if b==s:
-    print("True")
+n = input()
+m = n[::-1]
+if m==n:
+    print(True)
 else:
-    print("False")
+    print(False)
