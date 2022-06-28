64e78a3903b5f350e350c54e33fb2f3e400ec77c
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-06-28 20:14:44 +0530

    x = input() a = int(x)**2 y = x[::-1] b = int(y)**2 c = str(b) d = c[::-1] if str(a)==d:     print(True) else:     print(False)

--- Adam_number.py
+++ Adam_number.py
@@ -1,17 +1,10 @@
-x = eval(input())
-n = x**2
-s=0
-while x!=0:
-    v = x%10
-    s = s*10+v
-    x=x//10
-m = s**2
-a=0
-while m!=0:
-    c = m%10
-    a = a*10+c
-    m = m//10
-if n==a:
-    print("True")
+x = input()
+a = int(x)**2
+y = x[::-1]
+b = int(y)**2
+c = str(b)
+d = c[::-1]
+if str(a)==d:
+    print(True)
 else:
-    print("False")
+    print(False)
