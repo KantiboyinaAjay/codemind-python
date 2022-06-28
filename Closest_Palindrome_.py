64c0c0afb2ae1e04c2dfcb4c341a3632937fd225
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-06-28 14:01:41 +0530

    def palin(n):     n = str(n)     m = n[::-1]     if m==n:         return 1     return 0 a = int(input()) for i in range(a-1,0,-1):     if palin(i):         n = i         break j = a+1 while True:     if palin(j):         m = j         break     j+=1 if (a-n)==(m-a):     print(n,end=" ")     print(m) elif (a-n)<(m-a):     print(n) else:     print(m)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,28 +1,24 @@
-def is_palin(j): 
-
-    m = j
-    s=0
-    while m!=0:
-        v = m%10
-        s = s*10+v
-        m = m//10
-    if j==s:
-        return j
-
-x = int(input())
-for i in range(x-1,1,-1):
-    if is_palin(i):
-        a = i
+def palin(n):
+    n = str(n)
+    m = n[::-1]
+    if m==n:
+        return 1
+    return 0
+a = int(input())
+for i in range(a-1,0,-1):
+    if palin(i):
+        n = i
         break
-g = x+1
-while g!=0:
-    if is_palin(g):
-        b = g
+j = a+1
+while True:
+    if palin(j):
+        m = j
         break
-    g+=1
-if (x-a)<(b-x):
-    print(a)
-elif (x-a)==(b-x):
-    print(a,b)
+    j+=1
+if (a-n)==(m-a):
+    print(n,end=" ")
+    print(m)
+elif (a-n)<(m-a):
+    print(n)
 else:
-    print(b)
+    print(m)
