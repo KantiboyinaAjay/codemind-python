a6b28bc4dd228082b5bfdd881eb6ed78165e97cc
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-06-28 20:11:30 +0530

    x ,y = map(int,input().split()) for i in range(1,x+1):     if x%i==0:         if y%i==0:             m = i print(m)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,18 +1,6 @@
-x , y = map(int,input().split())
-l=[]
-m=[]
-h=[]
+x ,y = map(int,input().split())
 for i in range(1,x+1):
     if x%i==0:
-        l.append(i)
-for j in range(1,y+1):
-    if y%j==0:
-        m.append(j)
-for k in l:
-    for n in m:
-        if k==n:
-           h.append(k)
-if h!=0:
-    print(max(h))
-else:
-    print('-1')
+        if y%i==0:
+            m = i
+print(m)
