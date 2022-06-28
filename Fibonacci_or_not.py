abb6d576cdbf52d5051b412817d9aa3dc561a538
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-06-28 19:55:28 +0530

    x = int(input()) if x==0:     print(True) a,b=1,0 for i in range(x-1):     c = a+b     a,b=b,c     if x==c:         print(True)         break else:     print(False)

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,14 +1,12 @@
-x = eval(input())
-a = 0
-b = 1
+x = int(input())
 if x==0:
-    print("True")
-c = a+b
-while c<x:
-    c = a + b
-    a = b 
-    b = c
-if x==c:
-    print("True")
+    print(True)
+a,b=1,0
+for i in range(x-1):
+    c = a+b
+    a,b=b,c
+    if x==c:
+        print(True)
+        break
 else:
-    print("False")
+    print(False)
