03bb0f7f5d285df2c269fdc7143403479d541c83
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-06-28 20:32:47 +0530

    x = int(input()) n = int(x**0.5) c=0 for i in range(1,n+1):     if i*i==x:         print(True)         c=1 if c==0:     print(False)

--- perfect_square_root_or_not.py
+++ perfect_square_root_or_not.py
@@ -1,10 +1,9 @@
-from math import *
 x = int(input())
-for i in range(1,int(sqrt(x))+1):
-    c=0
+n = int(x**0.5)
+c=0
+for i in range(1,n+1):
     if i*i==x:
+        print(True)
         c=1
-if c==1:
-    print("True")
-else:
-    print("False")
+if c==0:
+    print(False)
