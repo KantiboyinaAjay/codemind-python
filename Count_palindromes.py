a1f125f9d1fcface32230c66a9c1b1f878eac51c
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-06-29 15:19:44 +0530

    def palin(n):     a=n     s=0     if n<0:         n=n*-1     while n!=0:         i = n%10         s = s*10+i         n = n//10     if a==s:         return 1     else:         return 0 x = int(input()) l = list(map(int,input().split())) c=0 for i in l:     if palin(i):         c+=1 print(c)

--- Count_palindromes.py
+++ Count_palindromes.py
@@ -1,8 +1,16 @@
-def palin(i):
-    s = str(i)
-    m = s[::-1]
-    if m==s:
+def palin(n):
+    a=n
+    s=0
+    if n<0:
+        n=n*-1
+    while n!=0:
+        i = n%10
+        s = s*10+i
+        n = n//10
+    if a==s:
         return 1
+    else:
+        return 0
 x = int(input())
 l = list(map(int,input().split()))
 c=0
