e9b3149dd1ac5287d22e512b8262bc2628e2f61c
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-06-29 15:37:33 +0530

    x = int(input()) l = list(map(str,input().split())) c=0 for i in l:     for j in i:         c+=int(j) print(c)

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,9 +1,7 @@
 x = int(input())
-l = list(map(int,input().split()))
-s=0
+l = list(map(str,input().split()))
+c=0
 for i in l:
-    while i!=0:
-        v=i%10
-        i=i//10
-        s+=v
-print(s)
+    for j in i:
+        c+=int(j)
+print(c)
