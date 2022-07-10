098110d4d57a47cfd2eff1bacecb01ab873a5dbe
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-07-10 15:37:24 +0530

    x = list(map(str,input().lower().split())) y = list(map(str,input().lower().split())) s=0 l=[] m=[] for i in x:     if i in y:         l.append(i) for i in y:     if i in x:         m.append(i) for i in l:     if i in m:         s+=1 print(s)

--- common_words_-1.py
+++ common_words_-1.py
@@ -1,7 +1,15 @@
 x = list(map(str,input().lower().split()))
 y = list(map(str,input().lower().split()))
-c=0
+s=0
+l=[]
+m=[]
 for i in x:
-    if y.count(i)==1:
-        c+=1
-print(c)
+    if i in y:
+        l.append(i)
+for i in y:
+    if i in x:
+        m.append(i)
+for i in l:
+    if i in m:
+        s+=1
+print(s)
