ae6263a1a773fbe7b073f1dc34ed01a9fdca7dca
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-07-10 10:40:28 +0530

    z = input().lower() s=0 for i in z:     if i>='a' and i<='z':         if z.count(i)==1:             s=1             print(i)             break if s==0:     print('-1')

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,9 +1,10 @@
-x = input()
-c=0
-for i in sorted(set(x),key=x.index):
-    if x.count(i)==1:
-        print(i)
-        c=1
-        break
-if c==0:
+z = input().lower()
+s=0
+for i in z:
+    if i>='a' and i<='z':
+        if z.count(i)==1:
+            s=1
+            print(i)
+            break
+if s==0:
     print('-1')
