daab68fcf6e62cfe5b264e65130a86319f56580f
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-07-10 14:06:37 +0530

    x=input() l = ['a','e','i','o','u'] m = ['A','E','I','O','U'] k=[] j=[] for i in x:     if i>='a' and i<='z':         if i in l:             k.append(i)     else:         if i in m:             j.append(i) k = set(k) j = set(j) if len(k)==5 or len(j)==5:     print(True) else:     print(False)

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,32 +1,18 @@
-x = input()
-m=[]
-l=[]
+x=input()
+l = ['a','e','i','o','u']
+m = ['A','E','I','O','U']   
+k=[]
+j=[]
 for i in x:
-    if i=='a':
-        l.append('a')
-    elif i=='e':
-        l.append('e')
-    elif i=='i':
-        l.append('i')
-    elif i=='o':
-        l.append('o')
-    elif i=='u':
-        l.append('u')
-    elif i=='A':
-        m.append('A')
-    elif i=='E':
-        m.append('E')
-    elif i=='I':
-        m.append('I')
-    elif i=='O':
-        m.append('O')
-    elif i=='U':
-        m.append('U')
-l = set(l)
-m = set(m)
-if len(l)==5:
-    print(True)
-elif len(m)==5:
+    if i>='a' and i<='z':
+        if i in l:
+            k.append(i)
+    else:
+        if i in m:
+            j.append(i)
+k = set(k)
+j = set(j)
+if len(k)==5 or len(j)==5:
     print(True)
 else:
     print(False)
