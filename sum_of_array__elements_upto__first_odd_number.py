d0104456ac7600194068a7e85dbec0223c37e4c0
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-07-17 16:38:56 +0530

    x=int(input()) l=list(map(int,input().split())) s=0 for i in l:     if i%2==1:         break     s+=i print(s)

--- sum_of_array__elements_upto__first_odd_number.py
+++ sum_of_array__elements_upto__first_odd_number.py
@@ -1,9 +1,8 @@
-x = int(input())
-l = list(map(int,input().split()))
-c=0
+x=int(input())
+l=list(map(int,input().split()))
+s=0
 for i in l:
-    c+=i
     if i%2==1:
-        d=i
         break
-print(c-d)
+    s+=i
+print(s)
