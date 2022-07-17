1c0fc3dd0d7e33e9772ba1322f4c23f43854b39b
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-07-17 17:05:41 +0530

    x=int(input()) l=list(map(int,input().split())) c=0 for i in set(l):     if i==l.count(i):         c+=1 print(c)

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -1,7 +1,7 @@
-x = int(input())
-l = list(map(int,input().split()))
+x=int(input())
+l=list(map(int,input().split()))
 c=0
-for i in list(set(l)):
-    if l.count(i)==i:
+for i in set(l):
+    if i==l.count(i):
         c+=1
 print(c)
