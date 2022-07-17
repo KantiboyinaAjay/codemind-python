8342f7ac72dacdc6c55d90870812681ece862a85
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-07-17 17:57:41 +0530

    x=int(input()) y=list(map(int,input().split())) i=0 s=0 for j in range(x-1,-1,-1):     s+=(2**i)*y[j]     i+=1 print(s)

--- Binary_Array_to_decimal.py
+++ Binary_Array_to_decimal.py
@@ -1,8 +1,8 @@
-x = int(input())
-l = list(map(int,input().split()))
-j=0
-v=0
-for i in range(x-1,-1,-1):
-    v+=(l[i]*(2**j))
-    j+=1
-print(v)
+x=int(input())
+y=list(map(int,input().split()))
+i=0
+s=0
+for j in range(x-1,-1,-1):
+    s+=(2**i)*y[j]
+    i+=1
+print(s)
