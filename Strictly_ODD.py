fde54bdddbc7000771a44d1ac971f981b853d0c5
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-07-17 18:23:25 +0530

    x = int(input()) l = list(map(int,input().split())) c,d=0,0 for i in range(x):     if i%2!=0 and l[i]%2==1:         c+=1     if l[i]%2==1:         d+=1 if c==d:     print(True) else:     print(False)

--- Strictly_ODD.py
+++ Strictly_ODD.py
@@ -2,9 +2,9 @@ x = int(input())
 l = list(map(int,input().split()))
 c,d=0,0
 for i in range(x):
-    if i%2!=0 and l[i]%2!=0:
+    if i%2!=0 and l[i]%2==1:
         c+=1
-    if l[i]%2!=0:
+    if l[i]%2==1:
         d+=1
 if c==d:
     print(True)
