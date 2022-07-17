0fc9c987d78fe227ae69097ce11cfceadb56dfb2
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-07-17 13:33:46 +0530

    x1=input().lower() x2=input().lower() l,s,m=[],[],[] for i in x1:     if i.isalnum():         l.append(i) for i in x2:     if i.isalnum():         s.append(i) for i in l:     if i not in s:         m.append(i) for i in s:     if i not in l:         m.append(i) m=set(m) print(len(m))

--- Non_common_characters.py
+++ Non_common_characters.py
@@ -1,14 +1,18 @@
-x = input().lower()
-y = input().lower()
-l=[]
-c=0
-for i in x:
-    if i not in y:
+x1=input().lower()
+x2=input().lower()
+l,s,m=[],[],[]
+for i in x1:
+    if i.isalnum():
         l.append(i)
-for i in y:
-    if i not in x:
-        l.append(i)
-for j in sorted(set(l)):
-    if j!=' ':
-        c+=1
-print(c)
+for i in x2:
+    if i.isalnum():
+        s.append(i)
+for i in l:
+    if i not in s:
+        m.append(i)
+for i in s:
+    if i not in l:
+        m.append(i)
+m=set(m)
+print(len(m))
+
