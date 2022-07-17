d9bde0df95cbeabb138fa2cb5645c346abdab23a
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-07-17 17:07:22 +0530

    x=int(input()) l=list(map(int,input().split())) l=sorted(set(l),key=l.index) print(*l)

--- unique_elements.py
+++ unique_elements.py
@@ -1,5 +1,4 @@
-x = int(input())
-l = list(map(int,input().split()))
-j = sorted(set(l),key=l.index)
-for i in j:
-    print(i,end=" ")
+x=int(input())
+l=list(map(int,input().split()))
+l=sorted(set(l),key=l.index)
+print(*l)
