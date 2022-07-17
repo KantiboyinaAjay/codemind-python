ae7ffd5abae026de3b03426e9efbac4be8e58df3
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-07-17 19:01:08 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     return 1 x=int(input()) l=list(map(int,input().split())) c=0 a=int(input()) for i in l:     if i>=a:         if prime(i):             c+=1 print(c)

--- Primes_greater_than_k.py
+++ Primes_greater_than_k.py
@@ -1,17 +1,16 @@
 def prime(n):
     if n==1:
         return 0
-    for i in range(2,(n//2)+1):
+    for i in range(2,int(n**0.5)+1):
         if n%i==0:
             return 0
     return 1
-x = int(input())
-l = list(map(int,input().split()))
-k = int(input())
-b=[]
+x=int(input())
+l=list(map(int,input().split()))
 c=0
+a=int(input())
 for i in l:
-    if prime(i):
-        if i>=k:
+    if i>=a:
+        if prime(i):
             c+=1
 print(c)
