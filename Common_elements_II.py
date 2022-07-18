848e304f7eda148f1bbcecf6551d2ed1bbfd5960
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-07-18 10:53:11 +0530

    x,y=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) c=[] for i in sorted(set(a),key=a.index):     if i not in b:         c.append(i) for i in sorted(set(b),key=b.index):     if i not in a:         c.append(i) print(*c)

--- Common_elements_II.py
+++ Common_elements_II.py
@@ -1,9 +1,11 @@
-x , y = map(int,input().split())
-l = list(map(int,input().split()))
-m = list(map(int,input().split()))
-for i in sorted(set(l),key=l.index):
-    if m.count(i)==0:
-        print(i,end=" ")
-for j in sorted(set(m),key=m.index):
-    if l.count(j)==0:
-        print(j,end=" ")
+x,y=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=[]
+for i in sorted(set(a),key=a.index):
+    if i not in b:
+        c.append(i)
+for i in sorted(set(b),key=b.index):
+    if i not in a:
+        c.append(i)
+print(*c)
