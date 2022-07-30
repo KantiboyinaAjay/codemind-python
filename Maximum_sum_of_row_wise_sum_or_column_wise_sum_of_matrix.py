f585b1f3346067e10e4803e2505e2e138a17b069
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-07-30 17:14:36 +0530

    a,b=map(int,input().split()) s=0 m=[] for i in range(a):     l=list(map(int,input().split()))     m.append(l) print(sum(max(m)))

--- Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.py
+++ Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.py
@@ -1,6 +1,7 @@
 a,b=map(int,input().split())
+s=0
 m=[]
 for i in range(a):
-    u=list(map(int,input().split()))
-    m.append(u)
+    l=list(map(int,input().split()))
+    m.append(l)
 print(sum(max(m)))
